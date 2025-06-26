2396fa9b050c124cc875d62c20895e7d056d097e
Zeri-Time <blee.940604@gmail.com>
2025-06-26 18:14:44 +0900

    react first commit 0626

---
+++ front/react/0626/src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+
+function App () {
+  return (
+    <>
+    <Naver />
+    <TailWindTest />
+    </>
+  )
+}
+function Naver() {
+  return(
+    <a href='http://www.naver.com' target='_blank' title='네이버로 이동'>네이버</a>
+  )
+}
+function TailWindTest() {
+  return(
+    <div className='text-3xl font-bold text-red-500'>
+      Tailwind Test
+    </div>
+  )
+}
+
+export default App;
