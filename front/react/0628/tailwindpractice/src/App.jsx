db5bf6b5579942836ed93f298fa443595ab1f8c9
Zeri-Time <blee.940604@gmail.com>
2025-06-28 19:46:05 +0900

    tailWindPractice

---
+++ front/react/0628/tailwindpractice/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+ return(
+  <>
+  <div className='text-red-500 urderline hover:text-red-200'>tailwind</div>
+  <div id='container' className='flex w-[500px] h-[200px] border border-black items-center justify-center' >test</div>
+  </>
+ )
+}
+
+export default App
