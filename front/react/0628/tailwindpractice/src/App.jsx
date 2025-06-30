4e0445e33c69f1efb841c392b060c55fc66e6539
Zeri-Time <blee.940604@gmail.com>
2025-06-30 20:56:56 +0900

    DOM

--- front/react/0628/tailwindpractice/src/App.jsx
+++ front/react/0628/tailwindpractice/src/App.jsx
@@ -5,6 +5,8 @@ function App() {
   <>
   <div className='text-red-500 urderline hover:text-red-200'>tailwind</div>
   <div id='container' className='flex w-[500px] h-[200px] border border-black items-center justify-center' >test</div>
+
+
   </>
  )
 }
