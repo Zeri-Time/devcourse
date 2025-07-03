7280fadc38c4351a75483a8663d50920d15722d5
Zeri-Time <blee.940604@gmail.com>
2025-07-03 14:34:46 +0900

    todo prctice continue

--- front/react/0701/src/App6.jsx
+++ front/react/0701/src/App6.jsx
@@ -7,8 +7,11 @@ function App6() {
     { text: '할일2', completed: false },
     { text: '할일3', completed: false },
   ])
-  const [deletedTodos, setDeletedTodos] = useState([])
+
   const [inputValue, setInputValue] = useState('')
+  const [deletedTodos, setDeletedTodos] = useState([])
+  const [editIndex, setEditIndex] = useState(null)
+  const [editValue, setEditValue] = useState('')
 
   const handleSubmit = (event) => {
     event.preventDefault()
@@ -18,13 +21,6 @@ function App6() {
     }
   }
 
-  const handleDelete = (index) => {
-    const deleted = todos[index]
-    const newTodos = todos.filter((_, i) => i !== index)
-    setTodos(newTodos)
-    setDeletedTodos([...deletedTodos, deleted])
-  }
-
   const handleCheckboxChange = (index) => {
     setTodos(
       todos.map((todo, i) =>
@@ -32,9 +28,37 @@ function App6() {
       )
     )
   }
+
+  const handleDelete = (index) => {
+    const deleted = todos[index]
+    const newTodos = todos.filter((_, i) => i !== index)
+    setTodos(newTodos)
+    setDeletedTodos([...deletedTodos, deleted])
+    // 수정 모드 중 삭제 시 수정 상태 해제
+    if (editIndex === index) {
+      setEditIndex(null)
+      setEditValue('')
+    }
+  }
+
+  const handleEditSave = (index) => {
+    setTodos(
+      todos.map((todo, i) =>
+        i === index ? { ...todo, text: editValue } : todo
+      )
+    )
+    setEditIndex(null)
+    setEditValue('')
+  }
+
+  const handleEditCancel = () => {
+    setEditIndex(null)
+    setEditValue('')
+  }
+
   return (
     <>
-      <div className="flex border border white p-4 w-[400px] h-full ml-[50px] rounded-md">
+      <div className="flex border border-white p-4 w-[400px] h-full ml-[50px] rounded-md">
         <form onSubmit={handleSubmit}>
           <input
             type="text"
@@ -55,20 +79,52 @@ function App6() {
                     type="checkbox"
                     className="mr-2"
                     checked={todo.completed}
-                    onChange={() =>
-                      handleCheckboxChange(todos.findIndex((t) => t === todo))
-                    }
+                    onChange={() => handleCheckboxChange(index)}
                   />
-                  {todo.text}
-                  <button
-                    type="button"
-                    onClick={() =>
-                      handleDelete(todos.findIndex((t) => t === todo))
-                    }
-                    className="text-white rounded-md w-[80px] h-[40px] ml-3"
-                  >
-                    삭제
-                  </button>
+                  {editIndex === index ? (
+                    <>
+                      <input
+                        value={editValue}
+                        onChange={(e) => setEditValue(e.target.value)}
+                        className="border rounded p-1 mr-2"
+                      />
+                      <button
+                        type="button"
+                        onClick={() => handleEditSave(index)}
+                        className="bg-green-500 text-white rounded-md px-2 py-1 mr-1"
+                      >
+                        저장
+                      </button>
+                      <button
+                        type="button"
+                        onClick={handleEditCancel}
+                        className="bg-gray-400 text-white rounded-md px-2 py-1"
+                      >
+                        취소
+                      </button>
+                    </>
+                  ) : (
+                    <>
+                      {todo.text}
+                      <button
+                        type="button"
+                        onClick={() => handleDelete(index)}
+                        className="text-white rounded-md w-[80px] h-[40px] ml-3"
+                      >
+                        삭제
+                      </button>
+                      <button
+                        type="button"
+                        onClick={() => {
+                          setEditIndex(index)
+                          setEditValue(todo.text)
+                        }}
+                        className="text-white rounded-md ml-2 w-[80px] h-[40px]"
+                      >
+                        수정
+                      </button>
+                    </>
+                  )}
                 </li>
               ))}
           </ul>
@@ -97,6 +153,16 @@ function App6() {
                     className="bg-gray-200 hover:bg-gray-300 text-white rounded-md text-sm w-[80px] h-[40px] ml-3"
                   >
                     삭제
+                  </button>
+                  <button
+                    type="button"
+                    onClick={() => {
+                      setEditIndex(todos.findIndex((t) => t === todo))
+                      setEditValue(todo.text)
+                    }}
+                    className="bg-yellow-400 text-white rounded-md w-[80px] h-[40px] ml-2"
+                  >
+                    수정
                   </button>
                 </li>
               ))}
