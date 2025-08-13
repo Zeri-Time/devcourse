f44c98c8eaaedee51b4961c9e14c60e99064a570
Zeri-Time <blee.940604@gmail.com>
2025-08-13 16:02:16 +0900

    fix mapping

--- back/Spring/demo/src/main/java/com/example/demo/MainController.java
+++ back/Spring/demo/src/main/java/com/example/demo/MainController.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import org.springframework.web.bind.annotation.GetMapping;
 
 public class MainController {
+    @GetMapping("/")
     public void index() {
         System.out.println("index 메서드 호출됨");
     }
