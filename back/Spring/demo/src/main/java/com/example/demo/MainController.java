aeddfb58fbac06820cd56083c3b4493c354825d5
Zeri-Time <blee.940604@gmail.com>
2025-08-13 15:49:36 +0900

    main controller

---
+++ back/Spring/demo/src/main/java/com/example/demo/MainController.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+public class MainController {
+    public void index() {
+        System.out.println("index 메서드 호출됨");
+    }
+    
+    @GetMapping("/hello")
+    public void hello() {
+        System.out.println("hello 메서드 호출됨");
+    }
+}
