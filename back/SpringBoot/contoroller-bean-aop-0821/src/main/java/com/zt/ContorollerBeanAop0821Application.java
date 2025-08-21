17ac39d571924cdd9c49d2dd8583ef112044d286
Zeri-Time <blee.940604@gmail.com>
2025-08-21 11:47:14 +0900

    controller-bean-aop init

---
+++ back/SpringBoot/contoroller-bean-aop-0821/src/main/java/com/zt/ContorollerBeanAop0821Application.java
@@ -0,0 +1,13 @@
+package com.zt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContorollerBeanAop0821Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContorollerBeanAop0821Application.class, args);
+    }
+
+}
---
+++ back/SpringBoot/contoroller-bean-aop-0821/src/test/java/com/zt/ContorollerBeanAop0821ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContorollerBeanAop0821ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
