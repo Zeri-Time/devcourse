30eef7764f37ed334888994b516f58fdc3c07b6b
Zeri-Time <blee.940604@gmail.com>
2025-08-21 11:41:26 +0900

    transactional

--- back/SpringBoot/jpaFundamental0820/src/main/java/com/zt/domain/post/post/service/PostService.java
+++ back/SpringBoot/jpaFundamental0820/src/main/java/com/zt/domain/post/post/service/PostService.java
@@ -2,16 +2,17 @@ package com.zt.domain.post.post.service;
 
 import com.zt.domain.post.post.entity.Post;
 import com.zt.domain.post.post.repository.PostRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
-@Component
+@RequiredArgsConstructor
+@Service
 public class PostService {
 
-    @Autowired
-    private PostRepository postRepository;
+    private final PostRepository postRepository;
 
     public Post save(Post post) {
         return postRepository.save(post);
--- back/SpringBoot/jpaFundamental0820/src/main/java/com/zt/global/initData/BaseInitData.java
+++ back/SpringBoot/jpaFundamental0820/src/main/java/com/zt/global/initData/BaseInitData.java
@@ -2,40 +2,49 @@ package com.zt.global.initData;
 
 import com.zt.domain.post.post.entity.Post;
 import com.zt.domain.post.post.service.PostService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
+@RequiredArgsConstructor
 @Configuration
 public class BaseInitData {
+    @Lazy
     @Autowired
-    private PostService postService;
+    private BaseInitData self;
+
+    private final PostService postService;
 
     @Bean
     ApplicationRunner baseInitDataApplicationRunner() {
         return args -> {
-            work1();
-            work2();
+            self.work1();
+            self.work2();
         };
     }
 
+    @Transactional
     void work1() {
         if (postService.count() > 0) return;
 
-        Post post1 = postService.save( new Post("제목 1", "내용 1"));
+        Post post1 = postService.save(new Post("제목 1", "내용 1"));
         // INSERT INTO post SET title = '제목 1';
         Post post2 = postService.save(new Post("제목 2", "내용 2"));
         // INSERT INTO post SET title = '제목 2';
 
-        System.out.println("post1.getId() : " +  post1.getId());
-        System.out.println("post2.getId() : " +  post2.getId());
+        System.out.println("post1.getId() : " + post1.getId());
+        System.out.println("post2.getId() : " + post2.getId());
 
         System.out.println("기본 데이터가 초기화 되었습니다.");
     }
 
+    @Transactional(readOnly = true)
     void work2() {
         Optional<Post> opPost = postService.findById(1);
         // SELECT * FROM post WHERE id = 1;
