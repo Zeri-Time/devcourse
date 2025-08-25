8dcff8c203f788ea9ce154567444e11770a68c8a
Zeri-Time <blee.940604@gmail.com>
2025-08-25 11:22:29 +0900

    repository 분리 수정

--- back/SpringBoot/contoroller-bean-aop-0821/src/main/java/com/zt/domain/wiseSaying/repository/WiseSayingRepository.java
+++ back/SpringBoot/contoroller-bean-aop-0821/src/main/java/com/zt/domain/wiseSaying/repository/WiseSayingRepository.java
@@ -1,22 +1,22 @@
 package com.zt.domain.wiseSaying.repository;
 
 import com.zt.domain.wiseSaying.entity.WiseSaying;
-import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-@Repository
+@Component
 public class WiseSayingRepository {
     private int lastId = 0;
     private final List<WiseSaying> wiseSayings = new ArrayList<>();
 
-    public WiseSaying save(String content, String author) {
-        int id = ++lastId;
-        WiseSaying wiseSaying = new WiseSaying(id, content, author);
-        wiseSayings.add(wiseSaying);
-        return wiseSaying;
+    public void save(WiseSaying wiseSaying) {
+        if (wiseSaying.isNew()) {
+            wiseSaying.setId(++lastId);
+            wiseSayings.add(wiseSaying);
+        }
     }
 
     public List<WiseSaying> findAll() {
@@ -24,10 +24,17 @@ public class WiseSayingRepository {
     }
 
     public Optional<WiseSaying> findById(int id) {
-        return wiseSayings.stream().filter(w -> w.getId() == id).findFirst();
+        return wiseSayings
+                .stream()
+                .filter(wiseSaying -> wiseSaying.getId() == id)
+                .findFirst();
     }
 
     public void delete(WiseSaying wiseSaying) {
         wiseSayings.remove(wiseSaying);
+    }
+
+    public long count() {
+        return wiseSayings.size();
     }
 }
--- back/SpringBoot/contoroller-bean-aop-0821/src/main/java/com/zt/domain/wiseSaying/service/WiseSayingService.java
+++ back/SpringBoot/contoroller-bean-aop-0821/src/main/java/com/zt/domain/wiseSaying/service/WiseSayingService.java
@@ -2,31 +2,21 @@ package com.zt.domain.wiseSaying.service;
 
 import com.zt.domain.wiseSaying.entity.WiseSaying;
 import com.zt.domain.wiseSaying.repository.WiseSayingRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+@RequiredArgsConstructor
 @Service
 public class WiseSayingService {
     private final WiseSayingRepository wiseSayingRepository;
 
-    public WiseSayingService(WiseSayingRepository wiseSayingRepository) {
-        this.wiseSayingRepository = wiseSayingRepository;
-    }
-
-    private int lastId = 0;
-    private final List<WiseSaying> wiseSayings = new ArrayList<>() {{
-        add(new WiseSaying(++lastId, "명언 1", "작가 1"));
-        add(new WiseSaying(++lastId, "명언 2", "작가 2"));
-        add(new WiseSaying(++lastId, "명언 3", "작가 3"));
-        add(new WiseSaying(++lastId, "명언 4", "작가 4"));
-        add(new WiseSaying(++lastId, "명언 5", "작가 5"));
-    }};
-
     public WiseSaying write(String content, String author) {
-        return wiseSayingRepository.save(content, author);
+        WiseSaying wiseSaying = new WiseSaying(content, author);
+        wiseSayingRepository.save(wiseSaying);
+        return wiseSaying;
     }
 
     public List<WiseSaying> findAll() {
@@ -43,7 +33,12 @@ public class WiseSayingService {
 
     public void modify(WiseSaying wiseSaying, String content, String author) {
         wiseSaying.modify(content, author);
+
+        wiseSayingRepository.save(wiseSaying);
     }
 
-
+    public long count() {
+        return wiseSayingRepository.count();
+    }
 }
+
