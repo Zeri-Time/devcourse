5cef4e394eea4c6a31712edf91f577782ceb46ba
Zeri-Time <blee.940604@gmail.com>
2025-08-26 11:40:58 +0900

    binding result

--- back/SpringBoot/p-14512/src/main/java/com/back/domain/post/post/controller/PostController.java
+++ back/SpringBoot/p-14512/src/main/java/com/back/domain/post/post/controller/PostController.java
@@ -1,11 +1,81 @@
 package com.back.domain.post.post.controller;
 
+import com.back.domain.post.post.entity.Post;
 import com.back.domain.post.post.service.PostService;
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @RequiredArgsConstructor
 @Controller
 public class PostController {
     private final PostService postService;
+
+    @GetMapping("/posts/write")
+    @ResponseBody
+    public String showWrite() {
+        return getWriteFormHtml("","", "", "");
+    }
+
+    @AllArgsConstructor
+    @Getter
+    public static class WriteForm {
+        @NotBlank(message = "제목을 입력해주세요.")
+        @Size(min = 2, max = 20, message = "제목은 2 ~ 10 자 이내로 입력해주세요.")
+        String title;
+
+        @NotBlank(message = "내용을 입력해주세요.")
+        @Size(min = 2, max = 100, message = "내용은 2 ~ 100 자 이내로 입력해주세요.")
+        String content;
+    }
+
+    @PostMapping("/posts/doWrite")
+    @ResponseBody
+    @Transactional
+    public String write(
+            @Valid WriteForm form, BindingResult bindingResult
+//        @ModelAttribute("writeForm") WriteForm form
+    ) {
+        if (bindingResult.hasErrors()) {
+
+            FieldError filedError = bindingResult.getFieldError();
+
+            String errorFiledName = filedError.getField();
+            String errorMessage = filedError.getDefaultMessage();
+
+            return getWriteFormHtml(errorFiledName, errorMessage, form.getTitle(), form.getContent());
+        }
+
+        Post post = postService.write(form.getTitle(), form.getContent());
+
+        return "%d 번 글이 생성 되었습니다.".formatted(post.getId());
+    }
+
+    private String getWriteFormHtml(
+            String errorFiledName,
+            String errorMessage,
+            String title,
+            String content
+    ) {
+        return """
+                <div style="color:red;">%s : %s</div>
+                <form action="/posts/doWrite" method="POST">
+                  <input type="text" name="title" placeholder="제목" value="%s" autofocus>
+                  <br>
+                  <textarea name="content" placeholder="내용">%s</textarea>
+                  <br>
+                  <input type="submit" value="작성">
+                </form>
+                """.formatted(errorFiledName, errorMessage, title, content);
+    }
 }
