e1084a13f66ed4ee807f3f545a25cbb8458fecbc
Zeri-Time <blee.940604@gmail.com>
2025-08-28 16:12:59 +0900

    mybatis

--- back/SpringBoot/my_batis/src/main/java/com/back/domain/member/repository/MemberRepository.java
+++ back/SpringBoot/my_batis/src/main/java/com/back/domain/member/repository/MemberRepository.java
@@ -1,7 +1,28 @@
 package com.back.domain.member.repository;
 
+import com.back.domain.member.dto.Member;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 @Mapper
 public interface MemberRepository {
+    List<Member>findAll();
+
+    Member findById(int id);
+
+    Member findByUsername(String username);
+
+    int save(Member member);
+
+    int deleteById(int id);
+
+    int update(
+            @Param("id") int id,
+            @Param("username") String username,
+            @Param("password") String password,
+            @Param("name") String name,
+            @Param("email") String email
+    );
 }
