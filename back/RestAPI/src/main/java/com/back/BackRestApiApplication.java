e7ca8e7e88b74afe8991ebb2a122ebc71b025914
Zeri-Time <blee.940604@gmail.com>
2025-09-03 10:53:39 +0900

    REST API init

---
+++ back/RestAPI/src/main/java/com/back/BackRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackRestApiApplication.class, args);
+    }
+
+}
